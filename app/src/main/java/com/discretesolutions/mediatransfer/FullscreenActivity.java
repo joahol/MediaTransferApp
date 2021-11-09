808921d4e92b3f14d377edc32ee5c741c780d212
joahol <31919454+joahol@users.noreply.github.com>
2021-11-09 09:47:24 +0100

    Some work on layouts, and bug fixes

--- app/src/main/java/com/discretesolutions/mediatransfer/FullscreenActivity.java
+++ app/src/main/java/com/discretesolutions/mediatransfer/FullscreenActivity.java
@@ -22,10 +22,16 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowInsets;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.discretesolutions.mediatransfer.databinding.ActivityFullscreenBinding;
 
+import java.util.Locale;
 import java.util.Set;
 
 /**
@@ -35,38 +41,130 @@ import java.util.Set;
 public class FullscreenActivity extends AppCompatActivity {
 
 
-
+Spinner modeSpinner;
+Spinner selectTypeSpinner;
 BluetoothAdapter bAdapter;
 ActivityResultLauncher aResLauncher;
+private Button btnConnect;
+private Button btnTransfer;
 private Set<BluetoothDevice> pairedBtDevices;
+private int SelectMode=0;
+private int DISPLAYMODE=0;
 
+//Constants for select and display modes
+private final int SELECTMODE_ALL=1;
+private final int SELECTMODE_SELECTED=2;
+private final int DISPLAYMODE_SMALLTHUMB=1;
+private final int DISPLAYMODE_LARGETHUMB=2;
+private final int DISPLAYMODE_LIST=3;
+
+
+private boolean avoid = true;//WHEN DEBUGGING IN EMULATOR SET THIS TO TRUE, DUE TO MISSING BLUETOOTH IN EMULATOR
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fullscreen);
-
         MediaStorageUnit msu = new MediaStorageUnit();
-
-        if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED){
-        Log.v("Permission:","Granted");
-
-            msu.queryMedia(this);
-        }else{
-            Log.v("Permission:","FAILED");
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.ACCESS_MEDIA_LOCATION},1);
-        }
-        final ImageListViewAdapter adapter = new ImageListViewAdapter(this,msu.getImageItems());
-        ListView lstv = (ListView) findViewById(R.id.lstItems);
-        lstv.setAdapter(adapter);
-        aResLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback() {
+        //Initialize Buttons
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnTransfer =(Button)findViewById(R.id.btnTransfer);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onActivityResult(Object result) {
-                Log.v("ActivityResult","some callback");
+            public void onClick(View view) {
+                Toast availBTDev = Toast.makeText(getApplicationContext(),"Bluetooth", 0x33);
+                availBTDev.setText("Connecting");
+                availBTDev.show();
+            }
+        });
+
+        btnTransfer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast availBTDev = Toast.makeText(getApplicationContext(),"Bluetooth", 0x33);
+                availBTDev.setText("Start transfer");
+                availBTDev.show();
+            }
+        });
+
+        //Initialize spinners
+        modeSpinner = (Spinner)findViewById(R.id.spinMode);
+        selectTypeSpinner=(Spinner)findViewById(R.id.spinSelect);
+        ArrayAdapter<CharSequence> arrAdaptMode = ArrayAdapter.createFromResource(this,R.array.listmode,R.layout.support_simple_spinner_dropdown_item);
+        arrAdaptMode.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        modeSpinner.setAdapter(arrAdaptMode);
+        ArrayAdapter<CharSequence> arrAdaptSelect = ArrayAdapter.createFromResource(this, R.array.spinnoptions, R.layout.support_simple_spinner_dropdown_item);
+        arrAdaptSelect.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        selectTypeSpinner.setAdapter(arrAdaptSelect);
+        selectTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+            String selected = adapterView.getItemAtPosition(pos).toString();
+
+                if (selected.equals("Selected")) {
+                    SelectMode = SELECTMODE_SELECTED;
+                } else if (selected.equals("All")) {
+                    SelectMode = SELECTMODE_ALL;
+                }
+                Log.v("Selected ",selected+" "+SelectMode);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
-        initialize();
+        modeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+            String selected = adapterView.getItemAtPosition(pos).toString();
+            Log.v("Selected ",selected);
 
+                if(selected.equals("Small thumbnail")){
+                    DISPLAYMODE=DISPLAYMODE_SMALLTHUMB;
+
+                }
+                else if(selected.equals("Large thumbnail")) {
+                    DISPLAYMODE=DISPLAYMODE_LARGETHUMB;
+
+                }
+                else if(selected.equals("List")){
+                    DISPLAYMODE=DISPLAYMODE_LIST;
+                }
+
+                Log.v("Selected ",selected+" "+DISPLAYMODE);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+//Check permisssions for reading from external storage
+        if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            Log.v("Permission:", "Granted");
+
+            msu.queryMedia(this);
+        } else {
+            Log.v("Permission:", "FAILED");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.ACCESS_MEDIA_LOCATION}, 1);
+        }
+
+        final ImageListViewAdapter adapter = new ImageListViewAdapter(this, msu.getImageItems());
+        ListView lstv = (ListView) findViewById(R.id.lstItems);
+        lstv.setAdapter(adapter);
+
+        //Bluetooth CODE section
+        if(!avoid) {
+            aResLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback() {
+        @Override
+        public void onActivityResult(Object result) {
+            Log.v("ActivityResult", "some callback");
+
+        }
+    });
+    initialize();
+}
 
 
     }
